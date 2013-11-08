68ee852075a34f3c82fcb0ca205d24d2ddcf1ab1
kolesofortuni <kolesofortuni@mail.ru>
2013-11-09 00:28:47 +0300

    Update 5-3.cpp

--- 5-3.cpp
+++ 5-3.cpp
@@ -4,7 +4,7 @@
 bool get_num(int64_t element, int bitnum);
 void partition(int64_t* a, int &i, int &j, int num);
 
-void binary_quick_sort(int64_t *a , int left , int right , int num) {
+void binquick_sort(int64_t *a , int left , int right , int num) {
     int i = left;
     int j = right;
     if ( right <= left || num < 0)
@@ -14,8 +14,8 @@ void binary_quick_sort(int64_t *a , int left , int right , int num) {
     if ( get_num (a[right] , num) == 0)
         j++;
 
-    binary_quick_sort(a, left, j-1, num-1);
-    binary_quick_sort(a, j, right, num-1);
+    binquick_sort(a, left, j-1, num-1);
+    binquick_sort(a, j, right, num-1);
 }
 
 void partition(int64_t* a, int &i, int &j, int num) {
@@ -37,13 +37,12 @@ int main() {
     int n = 0;
     int n64= 63;
     std::cin >> n;
-
     int64_t *a = new int64_t[n];
 
     for (int i = 0; i < n; i++)
         std::cin >> a[i];
 
-    binary_quick_sort(a, 0, n-1, n64);
+    binquick_sort(a, 0, n-1, n64);
 
     for (int i = 0; i < n; i++)
         std::cout << a[i] << " ";
